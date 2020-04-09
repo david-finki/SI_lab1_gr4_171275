bc752786a54d6e11b0caf49ffa5dc3cf52f55a64
wann4be <david.milanovski@students.finki.ukim.mk>
2020-04-09 19:59:22 +0200

    point class finished

--- Point.java
+++ Point.java
@@ -5,17 +5,25 @@ class Point {
 	//TODO add new variable
 
 	//TODO constructor
-
+	public Point(String id, double x, double y){
+		this.id=id;
+		this.x=x;
+		this.y=y;
+	}
 	//TODO setters and getters
+	public void setID(String id){this.id=id;}
+	public void setX(double x){this.x=x;}
+	public void setX(double y){this.y=y;}
 
 	public void move (char xDirection, char yDirection) {
-		//TODO
+		x+= xDirection=="L"?1:-1;
+		y+= yDirection=="U"?1:-1;
+		
+
 	}
 
 	public void draw () {
-		//TODO
+		System.out.println(this.x+" "+this.y);
 	}
-
-
 
 }
