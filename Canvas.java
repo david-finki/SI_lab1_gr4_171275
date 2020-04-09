71f52a5bbf7e64561bd3f41d840cf03047e81c42
wann4be <david.milanovski@students.finki.ukim.mk>
2020-04-09 20:07:51 +0200

    new class, changes to points.csv file

---
+++ Canvas.java
@@ -0,0 +1,22 @@
+class Canvas{
+
+    public List<Point> tocki = new List<Point>(101001010);
+
+    public Canvas(){
+
+    }
+
+    public void getTocka(){
+        //todo: implement
+    }
+
+    public void setTocka(){
+        //todo: implement
+    }
+
+    public Point getClosestTocka(){
+        //todo: implement
+    }
+
+
+}
